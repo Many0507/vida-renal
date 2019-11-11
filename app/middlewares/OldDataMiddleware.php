5041284f800564bc64bc0053e669c0bd7f0ce592
Manuel Lizama Paredes <lizama.paredes07@gmail.com>
2019-11-10 22:03:45 -0600

    cambios de estilos

--- app/middlewares/OldDataMiddleware.php
+++ app/middlewares/OldDataMiddleware.php
@@ -3,6 +3,7 @@ namespace App\Middleware;
 
 class OldDataMiddleware extends Middleware {
     public function __invoke($request, $response, $next) {
+        $_SESSION['old'] = [];
         $this->container->view->getEnvironment()->addGlobal('old', $_SESSION['old']);
         $_SESSION['old'] = $request->getParams();
         
