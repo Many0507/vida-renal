53d9eadcb9670b50df57f5900501925ce254f097
Manuel Lizama Paredes <manuel.lizama@yucatan.gob.mx>
2019-12-02 17:04:29 -0600

    base de datos

--- db/vida_renal.sql
+++ db/vida_renal.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-12-2019 a las 02:18:26
--- Versión del servidor: 10.1.37-MariaDB
--- Versión de PHP: 7.3.0
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 02-12-2019 a las 23:03:57
+-- Versión del servidor: 5.7.26
+-- Versión de PHP: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,14 +28,16 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `vr_actividades`
 --
 
-CREATE TABLE `vr_actividades` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `vr_actividades`;
+CREATE TABLE IF NOT EXISTS `vr_actividades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `texto` text CHARACTER SET utf32 COLLATE utf32_spanish_ci NOT NULL,
   `imagen` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `vr_actividades`
@@ -52,13 +54,15 @@ INSERT INTO `vr_actividades` (`id`, `titulo`, `texto`, `imagen`, `created_at`, `
 -- Estructura de tabla para la tabla `vr_admin`
 --
 
-CREATE TABLE `vr_admin` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `vr_admin`;
+CREATE TABLE IF NOT EXISTS `vr_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `pass` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
 
 --
 -- Volcado de datos para la tabla `vr_admin`
@@ -73,16 +77,18 @@ INSERT INTO `vr_admin` (`id`, `user`, `pass`, `created_at`, `updated_at`) VALUES
 -- Estructura de tabla para la tabla `vr_blog`
 --
 
-CREATE TABLE `vr_blog` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `vr_blog`;
+CREATE TABLE IF NOT EXISTS `vr_blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
   `texto` text CHARACTER SET utf8 COLLATE utf8_spanish_ci,
   `texto_corto` text CHARACTER SET utf8 COLLATE utf8_spanish_ci,
   `autor` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
   `imagen` varchar(120) CHARACTER SET utf32 COLLATE utf32_spanish_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `vr_blog`
@@ -101,21 +107,23 @@ INSERT INTO `vr_blog` (`id`, `titulo`, `texto`, `texto_corto`, `autor`, `imagen`
 -- Estructura de tabla para la tabla `vr_eventos`
 --
 
-CREATE TABLE `vr_eventos` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `vr_eventos`;
+CREATE TABLE IF NOT EXISTS `vr_eventos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `texto` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `imagen` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `vr_eventos`
 --
 
 INSERT INTO `vr_eventos` (`id`, `titulo`, `texto`, `imagen`, `created_at`, `updated_at`) VALUES
-(8, 'Evento 1', 'lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, aliquid eligendi modi dolorem sapiente explicabo quo officia numquam distinctio. Sunt dicta qui rem voluptatum veritatis autem adipisci exercitationem inventore atque.', '1f865a8605ea5bc5.jpg', '2019-11-25 10:22:08', '2019-11-25 10:22:08'),
+(8, 'Evento 1', 'lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, aliquid eligendi modi dolorem sapiente explicabo quo officia numquam distinctio. Sunt dicta qui rem voluptatum veritatis autem adipisci exercitationem inventore atque.', '5499728bb485d03a.jpg', '2019-11-25 10:22:08', '2019-12-03 04:05:15'),
 (9, 'Evento 2', 'lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, aliquid eligendi modi dolorem sapiente explicabo quo officia numquam distinctio. Sunt dicta qui rem voluptatum veritatis autem adipisci exercitationem inventore atque.', '58b5b79073e4dcd5.jpg', '2019-11-25 10:22:40', '2019-11-25 10:22:40'),
 (10, 'Evento 3', 'lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, aliquid eligendi modi dolorem sapiente explicabo quo officia numquam distinctio. Sunt dicta qui rem voluptatum veritatis autem adipisci exercitationem inventore atque.\r\n			', '6c1cededdbfa8c23.jpg', '2019-11-25 10:24:13', '2019-11-25 10:24:13');
 
@@ -125,14 +133,16 @@ INSERT INTO `vr_eventos` (`id`, `titulo`, `texto`, `imagen`, `created_at`, `upda
 -- Estructura de tabla para la tabla `vr_talleres`
 --
 
-CREATE TABLE `vr_talleres` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `vr_talleres`;
+CREATE TABLE IF NOT EXISTS `vr_talleres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `texto` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `imagen` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `vr_talleres`
@@ -146,102 +156,73 @@ INSERT INTO `vr_talleres` (`id`, `titulo`, `texto`, `imagen`, `created_at`, `upd
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `vr_testimonios`
+--
+
+DROP TABLE IF EXISTS `vr_testimonios`;
+CREATE TABLE IF NOT EXISTS `vr_testimonios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `texto` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `imagen` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `vr_testimonios`
+--
+
+INSERT INTO `vr_testimonios` (`id`, `titulo`, `texto`, `imagen`, `created_at`, `updated_at`) VALUES
+(1, 'Paola', '18 años enferma. Mi experiencia ha sido dura ya que es muy triste tener la enfermedad.', 'b57d25634e0b338d.jpg', '2019-12-02 18:39:43', '2019-12-03 01:29:41'),
+(3, 'Manuel', 'Descripción de testimonio', '93e7f18d9da71c36.jpg', '2019-12-03 03:48:02', '2019-12-03 03:48:02'),
+(4, 'Maria', 'Descripción de testimonio', 'bf6c919d7f24863b.jpg', '2019-12-03 03:48:18', '2019-12-03 03:48:18'),
+(5, 'Julia', 'Descripción de testimonio', '8122efadc62f7157.jpg', '2019-12-03 03:48:32', '2019-12-03 03:48:32');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `vr_video_principal`
 --
 
-CREATE TABLE `vr_video_principal` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `vr_video_principal`;
+CREATE TABLE IF NOT EXISTS `vr_video_principal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `video` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `vr_video_principal`
 --
 
 INSERT INTO `vr_video_principal` (`id`, `video`, `created_at`, `updated_at`) VALUES
-(1, '/public/video/video.mp4', '2019-12-01 23:46:37', '2019-12-01 23:46:37');
+(1, '/public/uploads/b923c16f51abeeb8.mp4', '2019-12-01 23:46:37', '2019-12-02 23:13:16');
+
+-- --------------------------------------------------------
 
 --
--- Índices para tablas volcadas
+-- Estructura de tabla para la tabla `vr_video_testimonio`
 --
 
---
--- Indices de la tabla `vr_actividades`
---
-ALTER TABLE `vr_actividades`
-  ADD PRIMARY KEY (`id`);
+DROP TABLE IF EXISTS `vr_video_testimonio`;
+CREATE TABLE IF NOT EXISTS `vr_video_testimonio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `video` varchar(120) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Indices de la tabla `vr_admin`
---
-ALTER TABLE `vr_admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `vr_blog`
---
-ALTER TABLE `vr_blog`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `vr_eventos`
---
-ALTER TABLE `vr_eventos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `vr_talleres`
---
-ALTER TABLE `vr_talleres`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `vr_video_principal`
---
-ALTER TABLE `vr_video_principal`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
+-- Volcado de datos para la tabla `vr_video_testimonio`
 --
 
---
--- AUTO_INCREMENT de la tabla `vr_actividades`
---
-ALTER TABLE `vr_actividades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
-
---
--- AUTO_INCREMENT de la tabla `vr_admin`
---
-ALTER TABLE `vr_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `vr_blog`
---
-ALTER TABLE `vr_blog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT de la tabla `vr_eventos`
---
-ALTER TABLE `vr_eventos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT de la tabla `vr_talleres`
---
-ALTER TABLE `vr_talleres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT de la tabla `vr_video_principal`
---
-ALTER TABLE `vr_video_principal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+INSERT INTO `vr_video_testimonio` (`id`, `video`, `created_at`, `updated_at`) VALUES
+(1, '/public/uploads/0332300f00a2e366.mp4', '2019-12-02 17:07:36', '2019-12-03 03:49:12');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
